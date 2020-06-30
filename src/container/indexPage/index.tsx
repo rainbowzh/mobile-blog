686c34307ec875e84e24250be04188c5ca6faf33
zhouhong07 <zhouhong07@58.com>
2020-06-30 19:53:13 +0800

    添加图片和完善滑动

--- src/container/indexPage/index.tsx
+++ src/container/indexPage/index.tsx
@@ -7,7 +7,7 @@ const IndexPage = () => {
       direction : 'vertical',
       autoplay: false,//可选选项，自动滑动
       // allowSlidePrev : false ,
-      touchRatio : 2
+      touchRatio : 0.6
     })
   },[]);
 
@@ -19,8 +19,13 @@ const IndexPage = () => {
             <img className="intro-typing" src="/assest/title.png"></img>
             <img className="intro-typing" src="/assest/title-2.png"></img>
             <img className="intro-typing" src="/assest/person-1.png"></img>
+            <img className="intro-typing" src="/assest/arrow.png"></img>
           </div>
-          <div className="indexpage-swiper-slide swiper-slide">2</div>
+          <div className="indexpage-swiper-slide swiper-slide">
+            <img className="intro-userMsg" src="/assest/userIntro-1.png" alt=""/>
+            <img className="intro-userMsg" src="" alt=""/>
+            <img className="intro-userMsg" src="" alt=""/>
+          </div>
           <div className="indexpage-swiper-slide swiper-slide">3</div>
           <div className="indexpage-swiper-slide swiper-slide">4</div>
           <div className="indexpage-swiper-slide swiper-slide">5</div>
